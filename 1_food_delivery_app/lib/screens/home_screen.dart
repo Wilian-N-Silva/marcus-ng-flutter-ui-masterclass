b24778f4eea7122ade858f8cc1f5fc2896f55279
Wilian Silva <wilian.silva209@gmail.com>
2020-02-03 01:01:18 -0300

    App UI 1 - Cart Screen Items + Quantity Selector

--- 1_food_delivery_app/lib/screens/home_screen.dart
+++ 1_food_delivery_app/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/data/data.dart';
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
+import 'package:flutter_food_delivery_ui/screens/cart_screen.dart';
 import 'package:flutter_food_delivery_ui/screens/restaurant_screen.dart';
 import 'package:flutter_food_delivery_ui/widgets/rating_stars.dart';
 import 'package:flutter_food_delivery_ui/widgets/recent_orders.dart';
@@ -100,13 +101,18 @@ class _HomeScreenState extends State<HomeScreen> {
         actions: <Widget>[
           FlatButton(
             child: Text(
-              'Card (${currentUser.cart.length})',
+              'Cart (${currentUser.cart.length})',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 20.0,
               ),
             ),
-            onPressed: () {},
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => CartScreen(),
+              ),
+            ),
           )
         ],
       ),
